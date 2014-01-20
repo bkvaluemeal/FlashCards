20bc6425a60f5a1fd0f5529ec0d45b7d24fdeb1f
bkvaluemeal <sirJustin.Willis@gmail.com>
2014-01-20 00:06:49 -0500

    Added main menu

--- FlashCards.java
+++ FlashCards.java
@@ -1,5 +1,34 @@
+/* FlashCards.java
+ * https://github.com/bkvaluemeal/FlashCards
+ */
+
+import java.util.Scanner;
+
 public class FlashCards {
+	private final static Scanner scanner = new Scanner(System.in);
+	private static String name = "";
+	private static int gameMode;
+	
+	private final static void mainMenu() {
+		System.out.println();
+		System.out.println("    1. Addition");
+		System.out.println("    2. Subtraction");
+		System.out.println("    3. Both");
+		System.out.println();
+	}
+	
 	public static void main(String args[]) {
+		// Gets user's name
+		System.out.print("Enter your name: ");
+		name = scanner.nextLine();
+		System.out.println();
 		
+		// Welcomes the user
+		System.out.println("Welcome " + name + ", please choose a game mode:");
+		mainMenu();
+		System.out.print("    > ");
+		
+		// Gets the game mode
+		gameMode = Integer.parseInt(scanner.nextLine());
 	}
 }
