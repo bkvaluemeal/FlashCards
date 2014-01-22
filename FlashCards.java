11cd51e95b8d55449e7615d911a60136ec595824
bkvaluemeal <sirJustin.Willis@gmail.com>
2014-01-21 22:43:09 -0500

    Added the game

--- FlashCards.java
+++ FlashCards.java
@@ -2,12 +2,15 @@
  * https://github.com/bkvaluemeal/FlashCards
  */
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class FlashCards {
 	private final static Scanner scanner = new Scanner(System.in);
+	private final static Random random = new Random();
 	private static String name = "";
 	private static int score = 0;
+	private static int lives = 5;
 	
 	/**
 	 * Prints the main menu
@@ -25,23 +28,27 @@ public class FlashCards {
 	/**
 	 * Prints the borders for the header
 	 * 
-	 * @param top
 	 * @param length
 	 */
-	private final static void doLine(boolean top, int length) {
-		if(top) {
-			System.out.print("╔");
-		} else {
-			System.out.print("╚");
-		}
+	private final static void doLine(int length) {
+		System.out.print("+");
 		for(int i = 2; i < length; i++) {
-			System.out.print("═");
+			System.out.print("-");
 		}
-		if(top) {
-			System.out.println("╗");
-		} else {
-			System.out.println("╝");
-		}
+		System.out.println("+");
+	}
+	
+	/**
+	 * Prints the correct splash
+	 */
+	private final static void correct() {
+		System.out.println();
+		System.out.println("   _____ ____  _____  _____  ______ _____ _______ ");
+		System.out.println("  / ____/ __ \\|  __ \\|  __ \\|  ____/ ____|__   __|");
+		System.out.println(" | |   | |  | | |__) | |__) | |__ | |       | |   ");
+		System.out.println(" | |   | |  | |  _  /|  _  /|  __|| |       | |   ");
+		System.out.println(" | |___| |__| | | \\ \\| | \\ \\| |___| |____   | |   ");
+		System.out.println("  \\_____\\____/|_|  \\_\\_|  \\_\\______\\_____|  |_|   ");
 	}
 	
 	/**
@@ -50,25 +57,78 @@ public class FlashCards {
 	 * @param gameMode
 	 */
 	private final static void loop(int gameMode) {
-		String header = "║ Score: " + score + " ║";
-		
-		// Print header
-		System.out.println();
-		System.out.println();
-		doLine(true, header.length());
-		System.out.println(header);
-		doLine(false, header.length());
-		
-		switch(gameMode) {
-		case 1:
-			System.out.println(1);
-			break;
-		case 2:
-			System.out.println(2);
-			break;
-		case 3:
-			System.out.println(3);
-			break;
+		while(lives > 0) {
+			boolean addition = random.nextBoolean();
+			
+			// Makes the header
+			String header = "| ";
+			if(addition || gameMode == 1) {
+				header += "Addition ";
+			} else {
+				header += "Subtraction ";
+			}
+			header += "| Score: " + score + " | Lives: " + lives + " |";
+			
+			int randA = random.nextInt(99);
+			int randB = random.nextInt(99);
+			int answer = 0;
+			
+			// Print header
+			System.out.println();
+			System.out.println();
+			doLine(header.length());
+			System.out.println(header);
+			doLine(header.length());
+			
+			switch(gameMode) {
+			case 1:
+				answer = randA + randB;
+				System.out.print(randA + " + " + randB + " = ");
+				try {
+					if(Integer.parseInt(scanner.nextLine().trim()) == answer) {
+						score++;
+						correct();
+					} else {
+						lives--;
+					}
+				} catch(Exception e) {
+					
+				}
+				break;
+			case 2:
+				answer = randA - randB;
+				System.out.print(randA + " - " + randB + " = ");
+				try {
+					if(Integer.parseInt(scanner.nextLine().trim()) == answer) {
+						score++;
+						correct();
+					} else {
+						lives--;
+					}
+				} catch(Exception e) {
+					
+				}
+				break;
+			case 3:
+				if(addition) {
+					answer = randA + randB;
+					System.out.print(randA + " + " + randB + " = ");
+				} else {
+					answer = randA - randB;
+					System.out.print(randA + " - " + randB + " = ");
+				}
+				try {
+					if(Integer.parseInt(scanner.nextLine().trim()) == answer) {
+						score++;
+						correct();
+					} else {
+						lives--;
+					}
+				} catch(Exception e) {
+					
+				}
+				break;
+			}
 		}
 	}
 	
@@ -107,5 +167,13 @@ public class FlashCards {
 		
 		// Starts the main loop
 		loop(gameMode);
+		
+		// Prints score
+		System.out.println();
+		if(score > 0) {
+			System.out.println("Good job " + name + "! You got " + score + " right.");
+		} else {
+			System.out.println("Wow " + name + ", L2Math! You got " + score + " right.");
+		}
 	}
 }
